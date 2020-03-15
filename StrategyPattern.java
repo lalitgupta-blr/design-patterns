c4ef3784132774fc54c841a3c83dc2cce468483f
Lalit Gupta <lalit.gupta@olodeck.com>
2020-03-15 13:31:01 +0530

    Adding Strategy Pattern Example

--- StrategyPattern.java
+++ StrategyPattern.java
@@ -1,92 +1,59 @@
-package strategy;
-
+import java.lang.Math;
 
 public class StrategyPattern {
 
-	public static void main(String[] args) { 
-	    MultiplicationStrategy strategy1 = new  BitWiseMultiplyStrategy();
-	    SimpleInterestCalculator calc1 = new SimpleInterestCalculator(strategy1);
-	    //principal=200, rateOfInterest=5% ,duration = 2 years
-	    int result1 = calc1.interestAmount(200, 5, 2);
-	    System.out.println("\n For BitWiseMultiplyStrategy ");
-	    System.out.println("\n Interest amount: " + result1);
-	    
-	    MultiplicationStrategy strategy2 = new  MultiplyOperatorStrategy();
-	    SimpleInterestCalculator calc2 = new SimpleInterestCalculator(strategy2);
-	   //principal=200, rateOfInterest=5% ,duration = 2 years
-	    int result2 = calc2.interestAmount(200, 5, 2);
-	    System.out.println("\n For MultiplyOperatorStrategy ");
-	    System.out.println("\n Interest amount: " + result2);
-	    
-	  }
+    public static void main(String[] args) {
+        SimpleIntrestOnBondsStrategy strategy1 = new  SimpleIntrestOnBondsStrategy();
+        MoneyIncreaseUsingBonds calc1 = new MoneyIncreaseUsingBonds(strategy1);
+        //principal=200, rateOfInterest=5% ,duration = 2 years
+        double result1 = calc1.interestAmount(200, 5, 2);
+        System.out.println("\n For SimpleIntrestOnBondsStrategy ");
+        System.out.println("\n Interest amount: " + result1);
+
+        CompoundIntrestOnBondsStrategy strategy2 = new  CompoundIntrestOnBondsStrategy();
+        MoneyIncreaseUsingBonds calc2 = new MoneyIncreaseUsingBonds(strategy2);
+        //principal=200, rateOfInterest=5% ,duration = 2 years
+        double result2 = calc2.interestAmount(200, 5, 2);
+        System.out.println("\n For CompoundIntrestOnBondsStrategy ");
+        System.out.println("\n Interest amount: " + result2);
+    }
 }
 
-interface  MultiplicationStrategy
+interface  BondsInterestStrategy
 {
-	int calculateMultiplication(int a, int b);
+    double calculateInterest(double principal, double rateOfInterest, double duration);
 }
 
-class BitWiseMultiplyStrategy implements MultiplicationStrategy
+
+class SimpleIntrestOnBondsStrategy implements BondsInterestStrategy
 {
-	@Override
-	public int calculateMultiplication(int x, int y){
-	        boolean neg = false;
-	        if(x < 0 && y >= 0){
-	            x = -x;
-	            neg = true;
-	        }
-	        else if(y < 0 && x >= 0){
-	            y = -y;
-	            neg = true;
-	        }else if( x < 0 && y < 0){
-	            x = -x;
-	            y = -y;
-	        }
-
-	        int res = 0;
-	        while(y!=0){
-	            if((y & 1) == 1) res += x;
-	            x <<= 1;
-	            y >>= 1;
-	        }
-	        return neg ? (-res) : res;
-	}
+    @Override
+    public double calculateInterest(double principal, double rateOfInterest, double duration){
+        return principal * rateOfInterest * duration/100;
+    }
 }
 
-class MultiplyOperatorStrategy implements MultiplicationStrategy
+class CompoundIntrestOnBondsStrategy implements BondsInterestStrategy
 {
-	@Override
-	public int calculateMultiplication(int x,int y){
-	       int res = 0, count = 0; 
-	       while (y > 0) 
-	        { 
-	            // check for set bit and left  
-	            // shift n, count times 
-	            if (y % 2 == 1)              
-	                res += x << count; 
-	      
-	            // increment of place  
-	            // value (count) 
-	            count++; 
-	            y /= 2; 
-	        } 
-	          
-	        return res;
-	}
+    @Override
+    public double calculateInterest(double principal, double rateOfInterest, double duration){
+        return principal * Math.pow (( 1+ rateOfInterest/100),duration) - principal;
+    }
 }
 
-
-class SimpleInterestCalculator
+class MoneyIncreaseUsingBonds
 {
-  private MultiplicationStrategy strategy;
+    private BondsInterestStrategy strategy;
 
-  public SimpleInterestCalculator(MultiplicationStrategy strategy)
-  {
-    this.strategy = strategy;
-  }
+    public MoneyIncreaseUsingBonds(BondsInterestStrategy strategy)
+    {
+        this.strategy = strategy;
+    }
 
-  public int interestAmount(int principal, int rateOfInterest, int duration)
-  {
-    return  strategy.calculateMultiplication(strategy.calculateMultiplication(principal, rateOfInterest),duration)/100;
-  }
+    public double interestAmount(double principal, double rateOfInterest, double duration)
+    {
+        return  strategy.calculateInterest(principal, rateOfInterest,duration);
+    }
 }
+
+
